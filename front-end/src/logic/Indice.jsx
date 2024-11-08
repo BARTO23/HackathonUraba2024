a0ba37a8654df300647806feee0d58ef15d48ab7
Andrés García <79813523+Ene44-A@users.noreply.github.com>
2024-11-07 22:30:32 -0500

    commit test

--- front-end/src/logic/Indice.jsx
+++ front-end/src/logic/Indice.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import DashBoardMain from "../views/DashBoardMain";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Facturas from "../components/Facturas";
 import { Pqr } from "../components/Pqr";
+import ClientesDashBoardMain from "../views/ClientesDashBoardMain";
+import FacturasDashBoardMain from "../views/FacturasDashBoardMain";
 
 export const Indice = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashBoardMain />} />
-        <Route path="/clientes" element={<DashBoardMain />} />
-        <Route path="/facturas" element={<Facturas />} />
+        <Route path="/clientes" element={<ClientesDashBoardMain />} />
+        <Route path="/facturas" element={<FacturasDashBoardMain />} />
         <Route path="/pqr" element={<Pqr />} />
       </Routes>
     </BrowserRouter>
